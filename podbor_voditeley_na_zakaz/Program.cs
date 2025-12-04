3677ade93df4a4dbe57b07ca29f14b0f75b80fb9
User <user@example.com>
2025-12-04 15:04:09 +0500

    TASK-1: Добавлены структуры данных

--- podbor_voditeley_na_zakaz/Program.cs
+++ podbor_voditeley_na_zakaz/Program.cs
@@ -1,1 +1,15 @@
-﻿Console.WriteLine("Hello, World!");
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+
+
+public record struct Point(int X, int Y);
+
+public record Driver(Guid id, Point CurrentLocation);
+
+public record Order(Point Location);
